0c7f163b3341e8e3d1cfcd58a8e0f8f7926a8f08
KSH <tmdgus6237@naver.com>
2024-06-12 19:56:14 +0900

    Delete .venv, Modify config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -44,9 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
-    'crispy_bootstrap4',
-
 ]
 
 CRISPY_TEMPLATE_PACK = 'crispy_bootstrap4'
